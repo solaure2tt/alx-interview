bc8697a2195340b3ac6a9d36ca5ffe2f83b47462
solaure2tt <solaure2t@yahoo.fr>
2023-10-19 09:26:28 -0700

    cleaning of the code

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,48 +3,6 @@
 import sys
 
 
-def validate_ip(s):
-    """ verify if s is a valid IP address"""
-    a = s.split('.')
-    if len(a) != 4:
-        return False
-    for x in a:
-        if not x.isdigit():
-            return False
-        i = int(x)
-        if i < 0 or i > 255:
-            return False
-    return True
-
-
-def verifyFormat(line):
-    """ verify if a format of a current line is correct
-        args:
-          line: lins to verify the format
-        return:
-          0 if the format is not correct otherwise 1
-    """
-    if line is None:
-        return 0
-    lineSplit = line.split()
-    """if len(lineSplit) != 6:
-        return 0
-    if validate_ip(lineSplit[0]) == False:
-        return 0
-    if lineSplit[1] != '-':
-        return 0
-    if isinstance(lineSplit[2], datetime.datetime) == False:
-        return 0
-    if lineSplit[3] != "GET /projects/260 HTTP/1.1":
-        return 0
-    status = [200, 301, 400, 401, 403, 404, 405, 500]
-    if lineSplit[4] not in status:
-        return 0
-    if isinstance(lineSplit[-1], int) == False:
-        return 0"""
-    return 1
-
-
 nblines = 0
 file_size = 0
 dict_sc = {"200": 0,
